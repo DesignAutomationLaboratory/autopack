879dcc9dfbc856d2d7e159c15a9f561559a28c18
Anton Wiberg <anton.wiberg@liu.se>
2023-09-21 09:29:47 +0200

    send bundleweighting to optimization

--- src/autopack/problem_setup.py
+++ src/autopack/problem_setup.py
@@ -9,7 +9,7 @@ class ProblemSetup():
 
 def harness_optimization(ips_instance, problem_setup, cost_field_weights, bundle_weight):
     new_field = cost_field.combine_cost_fields(problem_setup.cost_fields, cost_field_weights, normilise_fields = True)
-    new_harness = optimize_harness(ips_instance, problem_setup.harness_setup, new_field, bundle_weight=0.9)
+    new_harness = optimize_harness(ips_instance, problem_setup.harness_setup, new_field, bundle_weight=bundle_weight)
     
     bundle_cost, total_cost = harness.evaluate_harness(new_harness, problem_setup.cost_fields[0])
     return bundle_cost,total_cost
