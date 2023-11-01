a1193af7656da4bb51e597ddf33b90a6c57443c3
Olle Vidner <olle@vidner.se>
2023-11-01 19:41:46 +0100

    Add utility function for routing harness from dataset

--- src/autopack/harness_optimization.py
+++ src/autopack/harness_optimization.py
@@ -1,6 +1,7 @@
 import numpy as np
+import xarray as xr
 
-from autopack.data_model import CostField
+from autopack.data_model import CostField, IPSInstance, ProblemSetup
 from autopack.ips_communication.ips_commands import route_harness
 
 
@@ -71,3 +72,32 @@ def evaluate_harness(harness, cost_field):
             bundle_cost = bundle_cost + cost
             total_cost = total_cost + cost * len(segment.cables)
     return bundle_cost, total_cost
+
+
+def route_harness_from_dataset(
+    ips: IPSInstance,
+    ds: xr.Dataset,
+    case_id: str,
+    ips_solution_idx: int = 0,
+    build_discrete_solution=False,
+    build_presmooth_solution=False,
+    build_smooth_solution=False,
+):
+    problem_setup: ProblemSetup = ds.attrs["problem_setup"]
+
+    selected_ds = ds.sel(case=case_id, ips_solution=ips_solution_idx)
+    cost_field_weights = selected_ds["cost_field_weight"].values
+    bundling_factor = selected_ds["bundling_factor"].values
+
+    combined_cf = combine_cost_fields(problem_setup.cost_fields, cost_field_weights)
+
+    return route_harness(
+        ips_instance=ips,
+        harness_setup=problem_setup.harness_setup,
+        cost_field=combined_cf,
+        bundling_factor=bundling_factor,
+        harness_id=f"{case_id}_{ips_solution_idx}",
+        build_discrete_solution=build_discrete_solution,
+        build_presmooth_solution=build_presmooth_solution,
+        build_smooth_solution=build_smooth_solution,
+    )
