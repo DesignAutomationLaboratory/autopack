3fbf142ed1ae48a43abef939c9b348bf96841d0e
Olle Vidner <olle@vidner.se>
2023-12-11 15:58:46 +0100

    Make archive handling more robust

--- tasks.py
+++ tasks.py
@@ -42,17 +42,28 @@ def build(c, env_name=DEFAULT_ENV_NAME):
     pass
 
     conda_run(c, "pyinstaller --noconfirm autopack.spec")
-    bundle_path = pathlib.Path("dist/autopack")
+    build_path = pathlib.Path("build").resolve()
+    dist_path = pathlib.Path("dist").resolve()
+    bundle_path = dist_path / "autopack"
+    unversioned_archive_path = build_path / "autopack.7z"
+    versioned_archive_path = dist_path / f"autopack-v{ver}.7z"
 
     # The reason we use 7z is because the user might try to unzip the
     # file using the built-in Windows unzipper, which doesn't support
     # long file names.
 
-    archive_path = pathlib.Path(f"dist/archive/autopack-v{ver}.7z").resolve()
-    archive_path.unlink(missing_ok=True)
+    # Make sure that 7z does not append to an existing archive
+    unversioned_archive_path.unlink(missing_ok=True)
 
     with c.cd(bundle_path):
-        conda_run(c, f"7z a {archive_path} .")
+        # 7z handles dots in filenames really badly, so we're archiving
+        # into a safe filename and renaming afterwards
+        conda_run(c, f"7z a {unversioned_archive_path} .")
+
+    print(f"Moving archive to {versioned_archive_path}")
+    # Moving to an existing file will fail
+    versioned_archive_path.unlink(missing_ok=True)
+    unversioned_archive_path.rename(versioned_archive_path)
 
 
 @task
