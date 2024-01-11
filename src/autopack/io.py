7d9afe3c9d34505f6f6c0a3b36d32470a3a137b1
Olle Vidner <olle@vidner.se>
2024-01-11 19:20:26 +0100

    Pickle instead of using zarr

--- src/autopack/io.py
+++ src/autopack/io.py
@@ -1,9 +1,13 @@
-import facit
+import pickle
 
 
 def save_dataset(ds, path):
-    return facit.dump_zarr(ds, path)
+    with open(path, "wb") as file:
+        pickle.dump(ds, file)
+    return path
 
 
 def load_dataset(path):
-    return facit.load_zarr(path)
+    with open(path, "rb") as file:
+        ds = pickle.load(file)
+    return ds
