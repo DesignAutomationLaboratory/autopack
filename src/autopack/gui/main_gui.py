53b1782c12b072ad326b2996ea84e236ce207b00
Olle Vidner <olle@vidner.se>
2024-01-11 19:34:08 +0100

    Improve GUI and session management

--- src/autopack/gui/main_gui.py
+++ src/autopack/gui/main_gui.py
@@ -1,19 +1,70 @@
+import datetime
 import pathlib
+import shutil
+import tkinter as tk
+from tkinter import filedialog
 
 import holoviews as hv
+import hvplot.xarray  # noqa: F401
 import panel as pn
 import param
 
-from autopack import USER_DIR
+from autopack import USER_DIR, __version__
+from autopack.data_model import HarnessSetup
+from autopack.default_commands import create_default_prob_setup
+from autopack.harness_optimization import global_optimize_harness
+from autopack.io import load_dataset, save_dataset
 from autopack.ips_communication.ips_class import IPSInstance
+from autopack.ips_communication.ips_commands import load_scene, save_scene
 
 SETTINGS_PATH = USER_DIR / "gui-settings.json"
+SESSIONS_DIR = USER_DIR / "sessions"
+
+
+def make_session_name(problem_path: pathlib.Path):
+    problem_name = problem_path.stem
+    timestamp = datetime.datetime.utcnow()
+    timestamp_str = timestamp.strftime("%Y-%m-%dT%H%M%S")
+    return f"{problem_name}.{timestamp_str}.v{__version__}"
+
+
+def open_problem_path_dialog(
+    initial_dir=None,
+    title="Select a file",
+):
+    window = tk.Tk()
+    window.wm_attributes("-topmost", 1)
+    window.withdraw()
+    path = filedialog.askopenfilename(
+        parent=window,
+        initialdir=initial_dir,
+        title=title,
+        filetypes=[("JSON files", "*.json")],
+    )
+    window.destroy()
+    return pathlib.Path(path)
+
+
+def open_directory_dialog(
+    initial_dir=None,
+    title="Select a directory",
+):
+    window = tk.Tk()
+    window.wm_attributes("-topmost", 1)
+    window.withdraw()
+    path = filedialog.askdirectory(
+        parent=window,
+        initialdir=initial_dir,
+        title=title,
+    )
+    window.destroy()
+    return pathlib.Path(path)
 
 
 class Settings(param.Parameterized):
-    ips_path = param.Path(check_exists=False)
+    ips_path = param.Path(check_exists=False, label="IPS path")
     last_problem_path = param.Path(check_exists=False)
-    last_dataset_path = param.Path(check_exists=False)
+    last_session_path = param.Path(check_exists=False)
 
     @classmethod
     def load_or_new(cls):
@@ -22,7 +73,7 @@ class Settings(param.Parameterized):
         else:
             return cls()
 
-    @param.depends("ips_path", "last_problem_path", "last_dataset_path", watch=True)
+    @param.depends("ips_path", "last_problem_path", "last_session_path", watch=True)
     def persist(self):
         SETTINGS_PATH.parent.mkdir(parents=True, exist_ok=True)
         settings_to_persist = set(self.param.params().keys()) - {"name"}
@@ -32,7 +83,11 @@ class Settings(param.Parameterized):
 
 
 def create_settings_view(settings):
-    return pn.Param(settings, widgets={"ips_path": {"type": pn.widgets.TextInput}})
+    return pn.Param(
+        settings,
+        parameters=["ips_path"],
+        widgets={"ips_path": {"type": pn.widgets.TextInput}},
+    )
 
 
 class PostProcessor(param.Parameterized):
@@ -144,8 +199,13 @@ class VisualizationManager(param.Parameterized):
     def _update_dataset(self):
         self.post_processor.original_dataset = self.dataset
 
-    @param.depends("post_processor.processed_dataset", watch=True, on_init=True)
-    def _update_processed_dataset(self):
+    @param.depends(
+        "post_processor.processed_dataset",
+        "post_processor.processed_dataframe",
+        watch=True,
+        on_init=True,
+    )
+    def _update_processed_data(self):
         self.scatter_plot.dataset = self.post_processor.processed_dataset
         self.data_table.dataframe = self.post_processor.processed_dataframe
 
@@ -163,6 +223,12 @@ class VisualizationManager(param.Parameterized):
 class MainState(param.Parameterized):
     settings = param.ClassSelector(class_=Settings)
     viz_manager = param.ClassSelector(class_=VisualizationManager)
+
+    problem_path = param.Path()
+    session_path = param.Foldername()
+
+    run_problem_evt = param.Event(label="Run problem")
+    load_session_evt = param.Event(label="Load session")
 
     _ips = param.Parameter()
     dataset = param.Parameter()
@@ -182,6 +248,73 @@ class MainState(param.Parameterized):
     def _update_dataset(self):
         self.viz_manager.dataset = self.dataset
 
+    def session_manager_view(self):
+        problem_path_input = pn.widgets.TextInput.from_param(self.param.problem_path)
+        run_btn = pn.widgets.Button.from_param(self.param.run_problem_evt)
+
+        session_path_input = pn.widgets.TextInput.from_param(self.param.session_path)
+        load_btn = pn.widgets.Button.from_param(self.param.load_session_evt)
+
+        layout = pn.Column(
+            problem_path_input,
+            run_btn,
+            session_path_input,
+            load_btn,
+        )
+
+        return layout
+
+    @param.depends("run_problem_evt", watch=True)
+    def run_problem(self):
+        problem_path = pathlib.Path(self.problem_path)
+        session_name = make_session_name(problem_path)
+        session_path = SESSIONS_DIR / session_name
+        session_path.mkdir(parents=True, exist_ok=False)
+
+        self.settings.last_problem_path = problem_path
+
+        shutil.copy(problem_path, session_path / "setup.json")
+
+        harness_setup = HarnessSetup.from_json_file(problem_path)
+        problem_setup = create_default_prob_setup(
+            ips_instance=self.ips,
+            harness_setup=harness_setup,
+            create_imma=True,
+        )
+
+        dataset = global_optimize_harness(
+            ips_instance=self.ips,
+            problem_setup=problem_setup,
+            init_samples=2,
+            batches=2,
+            batch_size=2,
+        )
+
+        # Save the scene so we can inspect solutions later
+        save_scene(self.ips, session_path / "scene.ips")
+        save_dataset(dataset, session_path / "dataset.pkl")
+        session_marker = session_path / ".autopack-session"
+        session_marker.touch()
+
+        self.session_path = session_path
+        self.settings.last_session_path = session_path
+
+        self.dataset = dataset
+
+    @param.depends("load_session_evt", watch=True)
+    def load_session(self):
+        session_path = pathlib.Path(self.session_path)
+        session_marker = session_path / ".autopack-session"
+        if not session_marker.exists():
+            raise ValueError(
+                f"{self.session_path} is not a complete Autopack session directory"
+            )
+
+        self.dataset = load_dataset(session_path / "dataset.pkl")
+        load_scene(self.ips, session_path / "scene.ips", clear=True)
+
+        self.settings.last_session_path = session_path
+
 
 def make_gui(**main_state_kwargs):
     pn.extension("tabulator")
@@ -190,13 +323,18 @@ def make_gui(**main_state_kwargs):
     settings = Settings.load_or_new()
     viz_manager = VisualizationManager()
     main_state = MainState(
-        settings=settings, viz_manager=viz_manager, **main_state_kwargs
+        settings=settings,
+        viz_manager=viz_manager,
+        problem_path=settings.last_problem_path,
+        session_path=settings.last_session_path,
+        **main_state_kwargs,
     )
 
     return pn.template.BootstrapTemplate(
         title="Autopack",
         header_background="#b31f2f",  # IPS red
         sidebar=[
+            main_state.session_manager_view(),
             create_settings_view(settings),
         ],
         main=[viz_manager.view],
